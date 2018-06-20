7a0bcd9b8473052f127e51dadea3e2494dc0ed5a
Zhang Xu Ting <417760963@qq.com>
2018-06-20 14:51:48 +0800

    分类页面实现

--- js/categoryFirst.js
+++ js/categoryFirst.js
@@ -1,21 +1,50 @@
 $(function(){
      
-     $.ajax({
+     
+  var page = 1;
+  var pageSize = 5;
+  var totalpage = 0;
+  getData();
+  $('#prev').on('click',function(){
+       page--;
+       if(page < 1){
+       	  page = 1;
+       	  alert('已经是第一页了');
+       	  return;
+       }
+       getData();
+  })
+  $('#next').on('click',function(){
+  	  page++;
+  	  if(page > totalpage){
+          page = totalpage;
+          alert('没有更多数据了');
+          return;
+  	  }
+  	 getData(); 
+  })
+
+  function getData(){
+  	  $.ajax({
      	type: 'get',
      	url:  app.baseUrl + '/category/queryTopCategoryPaging',
      	data: {
-     		page: 1,
-     		pageSize: 10
+     		page: page,
+     		pageSize: pageSize
      	},
      	success: function(response){
      		console.log(response);
-     		
-     			  var html = template('categoryFirstTpl',response);
+     		 if(response.error){
+                 location.href = 'login.html';
+     		 }else {
+     		 	  var html = template('categoryFirstTpl',response);
      			  $('#categoryFirstBox').html(html);
-     	
+     	          totalpage = response.total/pageSize; 
+     		 } 
+     			  
      	}
      })
-
+  }
 
 
 
